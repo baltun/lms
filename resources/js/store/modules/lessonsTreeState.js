22a90afd4addb303c70a902b2b2efb846affe597
Ilya Kolesnikov <ivkol@mail.ru>
2021-05-30 17:09:06 +0300

    master - абсолютный путь к api поменял на относительный, убрал тестовые данные из метода получения дерева

--- resources/js/store/modules/lessonsTreeState.js
+++ resources/js/store/modules/lessonsTreeState.js
@@ -41,57 +41,8 @@ export default {
             return $layer;
         },
         getLeafs(ctx) {
-            const leafs = {
-                data: [{
-                    label: 'Урок 1',
-                    leaf: 'yes',
-                    children: [{
-                        label: 'Таблица 1-1',
-                        leaf: 'yes',
-                        children: [{
-                            label: 'Диаграмма 1-1-1'
-                        }],
-                        id: 1
-                    }]
-                }, {
-                    label: 'Урок 2',
-                    leaf: 'no',
-                    children: [{
-                        label: 'Таблица 2-1',
-                        leaf: 'yes',
-                        children: [{
-                            label: 'Диаграмма 2-1-1'
-                        }],
-                        id: 2,
-                    }, {
-                        label: 'Таблица 2-2',
-                        leaf: 'yes',
-                        children: [{
-                            label: 'Задание 2-2-1'
-                        }],
-                        leaf: 1
-                    }]
-                }, {
-                    label: 'Урок 3',
-                    children: [{
-                        label: 'Диаграмма 3-1',
-                        children: [{
-                            label: 'Таблица 3-1-1'
-                        }]
-                    }, {
-                        label: 'Таблица 3-2',
-                        children: [{
-                            label: 'Диаграмма 3-2-1'
-                        }]
-                    }]
-                }],
-                defaultProps: {
-                    children: 'children',
-                    label: 'label'
-                }
-            };
             axios
-                .get('http://twi.lo/api/lessons/tree')
+                .get('/api/lessons/tree')
                 .then(res => {
                     const leafs = res.data
                     console.log('leafs: '.leafs)
